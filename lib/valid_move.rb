b8de6e2e4ffb57a8e52f93f67c14b6baa4bfd9a1
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 05:58:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,5 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  # if !position_taken?(board, index)
-  #   return true
-  # elsif !index.between?(0, 8)
-  #   return false
   if index < 0 || index > 8
     return false
   elsif !position_taken?(board, index)
