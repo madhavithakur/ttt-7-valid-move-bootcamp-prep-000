4f872b36f7e9187937dbbd4477d1b53a846976da
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 05:58:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -19,19 +19,3 @@ def position_taken?(board, index)
   
 end 
   
-#   def on_board?(num)
-#     if num.between?(0, 8) == true
-#       return true
-#     else
-#       return false
-#     end
-#   end
-
-#   if (position_taken?(board, index)) == false && (on_board?(index) == true)
-#     return true
-#   else
-#     return false
-#   end
-
-
-# end
