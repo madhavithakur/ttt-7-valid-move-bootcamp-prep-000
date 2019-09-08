6edd44a93f8612184b739ab27687276d6cae8339
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 05:50:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move(board, index)
   if !position_taken?(index)
    return true
  else
-   
+   nil
 end 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
