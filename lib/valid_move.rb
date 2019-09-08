360f4e957c39d2fee766fd7947f60412a6528f45
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 05:56:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if !position_taken?(board, index)
     return true
-  elsif !board.include?(index)
+  elsif !index.between?(0, 8)
     return false
   else
     nil
@@ -18,16 +18,7 @@ def position_taken?(board, index)
   end 
   
 end 
-
-def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
-
+  
   def on_board?(num)
     if num.between?(0, 8) == true
       return true
